e3314242ff2a79f219c9f6c8889e8e69278cbd7e
shaajiiii <manushaji717@gmail.com>
2025-06-28 19:15:11 +0530

    bug fix

--- client/src/api/dashboard.ts
+++ client/src/api/dashboard.ts
@@ -2,6 +2,7 @@ import axios from "./axiosConfig";
 import type { QueryKey } from "../constants/dashboardCards";
 
 export const fetchDashboardData = async (query_key: QueryKey | null) => {
+  if (!query_key) return;
   const response = await axios.get("/dashboard", {
     params: { query_key },
   });
